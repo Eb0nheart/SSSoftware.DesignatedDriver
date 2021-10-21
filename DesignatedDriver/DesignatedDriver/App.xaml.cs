53062fcc7a4222cad5d0ec37918c8af8ca393f99
Eb0nheart <76154207+Eb0nheart@users.noreply.github.com>
2021-10-21 19:58:43 +0200

    Add project files.

---
+++ DesignatedDriver/DesignatedDriver/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace DesignatedDriver
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            NavigationPage navPage = new NavigationPage(new MainPage());
+            navPage.BarBackgroundColor = Color.DarkCyan;
+            MainPage = navPage;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ DesignatedDriver/DesignatedDriver/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DesignatedDriver
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            picker.ItemsSource = new List<string>() { "Male", "Female" };
+            ToolbarItems.Add(new ToolbarItem("Info", "", () =>
+            {
+                Navigation.PushAsync(new InfoPage());
+            }));
+        }
+        
+        private void calculateResult(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int intWeight = int.Parse(weight.Text);
+                int intHours = int.Parse(hours.Text);
+                bool male = picker.SelectedItem.ToString() == "Male";
+                double result;
+
+                if (male)
+                {
+                    result = ((0.5 + (0.15 * (intHours - 1))) * (intWeight * 0.7)) / 12;
+                }
+                else
+                {
+                    result = ((0.5 + (0.15 * (intHours - 1))) * (intWeight * 0.6)) / 12;
+                }
+
+                DisplayAlert((int)result + " Drinks Allowed!", "Remember to not drink alcohol atleast 1 hour before driving to sober up!", "Noice!");
+            }
+            catch
+            {
+                DisplayAlert("Error!", "Missing or incorrect inputs", "Okay");
+            }
+        }
+        
+    }
+}
